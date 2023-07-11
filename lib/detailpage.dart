90ebb655a63a8435ce58220b0dad4fe88718c711
t2023-m0076 <t2023-m0076@t2023-m0076ui-MacBookAir.local>
2023-07-11 17:54:39 +0900

    test

--- lib/detailpage.dart
+++ lib/detailpage.dart
@@ -28,88 +28,92 @@ class DetailPage extends StatelessWidget {
         ),
         body: Padding(
           padding: EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 15),
-              Container(
-                margin: EdgeInsets.only(left: 3, right: 3), // 좌우 여백 설정
-                child: Column(
-                  children: [
-                    Row(
+          child: Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 15),
+                  Container(
+                    margin: EdgeInsets.only(left: 3, right: 3), // 좌우 여백 설정
+                    child: Column(
                       children: [
-                        Container(
-                          margin: EdgeInsets.only(right: 10.0),
-                          decoration: BoxDecoration(
-                            border: Border.all(),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          alignment: Alignment.center,
-                          width: 250,
-                          height: 150,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "이름",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                ),
+                        Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(right: 10.0),
+                              decoration: BoxDecoration(
+                                border: Border.all(),
+                                borderRadius: BorderRadius.circular(20),
                               ),
-                              Text("MBTI"),
-                              Text("자신의 블로그 주소"),
-                            ],
-                          ),
+                              alignment: Alignment.center,
+                              width: 250,
+                              height: 150,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "이름",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  Text("MBTI"),
+                                  Text("자신의 블로그 주소"),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              alignment: Alignment.center,
+                              width: 148,
+                              height: 150,
+                              child: Text("사진"),
+                            ),
+                          ],
                         ),
+                        SizedBox(height: 10),
                         Container(
                           decoration: BoxDecoration(
                             border: Border.all(),
                             borderRadius: BorderRadius.circular(20),
                           ),
-                          alignment: Alignment.center,
-                          width: 148,
-                          height: 150,
-                          child: Text("사진"),
+                          width: double.infinity,
+                          height: 500,
+                          child: Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "자신의 스타일, 장점, 그 외 TMI",
+                                border: InputBorder.none,
+                              ),
+                              autofocus: true,
+                              maxLines: null,
+                              expands: true,
+                              keyboardType: TextInputType.multiline,
+                            ),
+                          ),
                         ),
                       ],
                     ),
-                    SizedBox(height: 10),
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      width: double.infinity,
-                      height: 500,
-                      child: Padding(
-                        padding: EdgeInsets.all(15.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: "자신의 스타일, 장점, 그 외 TMI",
-                            border: InputBorder.none,
-                          ),
-                          autofocus: true,
-                          maxLines: null,
-                          expands: true,
-                          keyboardType: TextInputType.multiline,
-                        ),
-                      ),
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 60,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text("수정하기"),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              SizedBox(height: 20),
-              SizedBox(
-                width: double.infinity,
-                height: 60,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text("수정하기"),
-                ),
-              ),
-            ],
+            ),
           ),
         ));
   }

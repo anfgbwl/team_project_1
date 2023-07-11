b60654371286b0ef6d9b7a38965c0a6cd3fd2c04
JeongKiKi <jkh001301@gmail.com>
2023-07-11 09:23:13 +0900

    상세페이지 프레임

---
+++ lib/detailpage.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.black,
+                  size: 20,
+                ))
+          ],
+        ),
+        title: Text(
+          "E1I4",
+          style: TextStyle(color: Colors.black, fontSize: 20),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.add,
+                color: Colors.black,
+              ))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 15,
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(),
+                              borderRadius: BorderRadius.circular(20)),
+                          alignment: Alignment.center,
+                          width: 200,
+                          height: 150,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "이름",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                              Text("MBTI"),
+                              Text("자신의 블로그 주소"),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(),
+                              borderRadius: BorderRadius.circular(20)),
+                          alignment: Alignment.center,
+                          width: 150,
+                          height: 150,
+                          child: Text("사진"),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 20, width: double.infinity),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(20)),
+                        width: double.infinity,
+                        height: 500,
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: "자신의 스타일, 장점, 그 외 TMI",
+                              border: InputBorder.none,
+                            ),
+                            autofocus: true,
+                            maxLines: null,
+                            expands: true,
+                            keyboardType: TextInputType.multiline,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 60,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text("수정하기"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
